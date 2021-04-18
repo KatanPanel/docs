294e20922724d8ddd16a2e7091b5275dab2c899a
DevNatan <itroken99@gmail.com>
2021-04-18 16:15:01 -0300

    chore: add PWA plugin and configure sitemap

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,77 +1,110 @@
 module.exports = {
-    title: 'Katan',
-    tagline: 'An open source game server management panel.',
-    url: 'https://katanpanel.github.io',
-    baseUrl: '/katan-website/',
-    onBrokenLinks: 'warn',
-    favicon: 'img/favicon.ico',
-    organizationName: 'KatanPanel',
-    projectName: 'katan-website',
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'pt-br'],
-        localeConfigs: {
-            en: {
-                label: 'English',
-            },
-            "pt-br": {
-                label: 'Português (Brasil)',
-            },
-        }
+  title: "Katan",
+  tagline: "An open source game server management panel.",
+  url: "https://katanpanel.github.io",
+  baseUrl: "/katan-website/",
+  onBrokenLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "KatanPanel",
+  projectName: "katan-website",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "br"],
+    localeConfigs: {
+      en: {
+        label: "English",
+      },
+      br: {
+        label: "Português (Brasil)",
+      },
     },
-    themeConfig: {
-        navbar: {
-            title: 'Katan',
-            logo: {
-                alt: 'Katan Logo',
-                src: 'img/logo.png',
-            },
-            items: [
-                {
-                    to: 'docs/',
-                    activeBasePath: 'docs',
-                    label: 'Documentation',
-                    position: 'right',
-                },
-                {
-                    href: 'https://discord.gg/ey8dwsv',
-                    label: 'Community',
-                    position: 'right',
-                },
-                {
-                    type: 'localeDropdown',
-                    position: 'right',
-                },
-                {
-                    href: 'https://github.com/KatanPanel',
-                    position: 'right',
-                    className: 'header-github-link',
-                    'aria-label': 'GitHub',
-                },
-            ],
+  },
+  themeConfig: {
+    hideableSidebar: true,
+    navbar: {
+      title: "Katan",
+      logo: {
+        alt: "Katan Logo",
+        src: "img/logo.png",
+      },
+      items: [
+        {
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Documentation",
+          position: "left",
         },
-        footer: {
-            style: 'dark',
-            copyright: `Copyright © ${new Date().getFullYear()} Katan, Inc. Built with Docusaurus.`,
+        {
+          href: "https://discord.gg/ey8dwsv",
+          label: "Community",
+          position: "left",
         },
-        gtag: {
-            trackingID: 'G-5HFBSRG9BY',
-            anonymizeIP: true,
-        }
+        {
+          type: "localeDropdown",
+          position: "right",
+        },
+        {
+          href: "https://github.com/KatanPanel",
+          position: "right",
+          className: "header-github-link",
+          "aria-label": "GitHub",
+        },
+      ],
     },
-    presets: [
-        [
-            '@docusaurus/preset-classic',
-            {
-                docs: {
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    editUrl:
-                        'https://github.com/KatanPanel/katan-website/edit/main/',
-                },
-                theme: {
-                    customCss: require.resolve('./src/css/custom.css'),
-                },
-            },
+    footer: {
+      style: "dark",
+      copyright: `Copyright © ${new Date().getFullYear()} Katan, Inc. Built with Docusaurus.`,
+    },
+    gtag: {
+      trackingID: "G-5HFBSRG9BY",
+      anonymizeIP: true,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/KatanPanel/katan-website/edit/main/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+        sitemap: {
+          changefreq: "weekly",
+          priority: 0.5,
+          trailingSlash: false,
+        },
+      },
+    ],
+    [
+      "@docusaurus/plugin-pwa",
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          "appInstalled",
+          "standalone",
+          "queryString",
         ],
-    ]
+        pwaHead: [
+          {
+            tagName: "link",
+            rel: "icon",
+            href: "/img/logo.png",
+          },
+          {
+            tagName: "link",
+            rel: "manifest",
+            href: "/manifest.json",
+          },
+          {
+            tagName: "meta",
+            name: "theme-color",
+            content: "#4b7bec",
+          },
+        ],
+      },
+    ],
+  ],
 };
